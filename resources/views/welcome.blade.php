1622d05be30725933f44b0e96961a413e32dacb3
Maxi <herreramaxi@gmail.com>
2021-01-30 17:39:43 +0000

    adding post build to heroku.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,13 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
 </head>
 
 <body>
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
