963ecd90dd8bd222b0b0243a748bf2f92d5f52d5
Maxi <herreramaxi@gmail.com>
2021-01-30 17:27:30 +0000

    first commit

---
+++ resources/js/components/App.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from "svelte";
+    import HelloWorld from "./HelloWorld.svelte";
+    import Ra from "./RA.svelte";
+
+    onMount(() => {
+        console.log("the component has mounted");
+    });
+</script>
+
+// App.svelte
+<main>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+                    <div class="card-body">I'm an example component.</div>
+                    <HelloWorld />
+                    <Ra />
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
---
+++ resources/js/components/HelloWorld.svelte
@@ -0,0 +1,5 @@
+<script>
+    let name = "max2";
+</script>
+
+<h1>Hello {name}!</h1>
